75e0172da8ae7d87c7689f98da6ee2eb58886519
Pranali <sawantpranali@gmail.com>
2020-11-06 15:04:29 +0530

    First commit

---
+++ Assignment1/src/assignment1/Assignment1.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author HP
+ */
+public class Assignment1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a,b;
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter Two Numbers");
+        a=sc.nextInt();
+        b=sc.nextInt();
+        if(a>b)
+        {
+            System.out.println("a="+a);
+            
+        }
+        else{
+            System.out.println("b="+b);
+        }
+        
+    }
+            
+            {
+     }
+    
+}
